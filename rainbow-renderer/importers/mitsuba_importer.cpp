a4f6e276c5b8d68ea962089487ff923aa80e1de0
Link Clinton <LinkClinton@outlook.com>
2020-05-13 17:32:09 +0800

    finish mi.xml test.

--- rainbow-renderer/importers/mitsuba_importer.cpp
+++ rainbow-renderer/importers/mitsuba_importer.cpp
@@ -9,17 +9,6 @@ namespace rainbow::renderer::importers {
 	renderer_scene import_mitsuba_scene(const std::string& filename)
 	{
 		const auto meta_scene = metascene::importers::mitsuba::import_mitsuba_scene(filename);
-
-		for (const auto& entity : meta_scene->entities) {
-			if (entity->emitter != nullptr)
-				logs::info(entity->emitter->to_string());
-
-			if (entity->material != nullptr)
-				logs::info(entity->material->to_string());
-
-			if (entity->shape != nullptr)
-				logs::info(entity->shape->to_string());
-		}
 		
 		return create_from_meta_scene(meta_scene);
 	}
--- rainbow-renderer/main.cpp
+++ rainbow-renderer/main.cpp
@@ -3,10 +3,8 @@
 using namespace rainbow;
 
 int main() {
-	const auto scene = rainbow::renderer::importers::import_mitsuba_scene("./../scenes/veach_mi/mi.xml");
+	const auto scene = renderer::importers::import_mitsuba_scene("./../scenes/veach_mi/mi.xml");
 
-	scene.integrator->set_debug_trace_pixel(vector2i(617, 224));
-	
 	scene.render_to("image");
 
 	system("pause");
