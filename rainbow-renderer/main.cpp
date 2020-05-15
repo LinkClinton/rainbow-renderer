c7f987aa639a6e954ee60fb01ecbd51c93e6b74a
Link Clinton <LinkClinton@outlook.com>
2020-05-15 12:35:43 +0800

    update

--- rainbow-renderer/main.cpp
+++ rainbow-renderer/main.cpp
@@ -1,11 +1,15 @@
 #include "importers/mitsuba_importer.hpp"
 
+#include "meta-scene/importers/pbrt/pbrt_importer.hpp"
+
 using namespace rainbow;
 
 int main() {
-	const auto scene = renderer::importers::import_mitsuba_scene("./../scenes/cbox/cbox.xml");
+	metascene::importers::pbrt::import_pbrt_scene("./../scenes/dragon/f8-10.pbrt");
+	
+	/*const auto scene = renderer::importers::import_mitsuba_scene("./../scenes/cbox/cbox.xml");
 
 	scene.render_to("image");
 
-	system("pause");
+	system("pause");*/
 }
